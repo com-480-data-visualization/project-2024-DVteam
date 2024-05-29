6e35d114039e3bd95770e2496edcf67ba80d85db
psotogomez <p.soto.g@uc.cl>
2024-05-29 16:55:31 +0200

    feat(merge)

--- docs/radial_vis.js
+++ docs/radial_vis.js
@@ -1,5 +1,6 @@
 function processData(csvFile, containerId, title) {
-    d3.csv(csvFile).then(data => {
+    d3.csv(csvFile, function (data) {
+    // d3.csv(csvFile).then(data => {
         data.forEach(d => {
             d['distance_ft'] = +d['distance_ft'];
             d['lebron_team_score'] = +d['lebron_team_score'];
@@ -8,16 +9,30 @@ function processData(csvFile, containerId, title) {
 
         const successfulShots = data.filter(d => d['result'].toLowerCase() === "true");
 
-        const shotCounts = d3.rollups(successfulShots, v => ({
-            twoPointShots: v.filter(d => d['shot_type'] === '2').length,
-            threePointShots: v.filter(d => d['shot_type'] === '3').length
-        }), d => d['date']);
 
-        const flatData = shotCounts.map(([date, counts]) => ({
-            date,
-            twoPointShots: counts.twoPointShots,
-            threePointShots: counts.threePointShots
+        const shotCounts = d3.nest()
+            .key(d => d.date)
+            .rollup(v => ({
+                twoPointShots: v.filter(d => d.shot_type === '2').length,
+                threePointShots: v.filter(d => d.shot_type === '3').length
+            }))
+            .entries(successfulShots)
+        // const shotCounts = d3.rollups(successfulShots, v => ({
+        //     twoPointShots: v.filter(d => d['shot_type'] === '2').length,
+        //     threePointShots: v.filter(d => d['shot_type'] === '3').length
+        // }), d => d['date']);
+
+        const flatData = shotCounts.map(({ key, value }) => ({
+            date: key,
+            twoPointShots: value.twoPointShots,
+            threePointShots: value.threePointShots
         }));
+
+        // const flatData = shotCounts.map(([date, counts]) => ({
+        //     date,
+        //     twoPointShots: counts.twoPointShots,
+        //     threePointShots: counts.threePointShots
+        // }));
 
         console.log(flatData);
 
@@ -32,8 +47,8 @@ function processData(csvFile, containerId, title) {
         flatData.sort((a, b) => new Date(a.date) - new Date(b.date));
 
         // Define SVG dimensions and radius for the rings
-        const width = 800;
-        const height = 1000;
+        const width = 650; //800
+        const height = 900; //1000
         const margin = { top: 50, right: 50, bottom: 50, left: 50 };
         const radius = 1.25 * (Math.min(width, height) / 2 - Math.max(margin.top, margin.right, margin.bottom, margin.left));
 
@@ -41,7 +56,7 @@ function processData(csvFile, containerId, title) {
         const svg = d3.select(containerId).append("svg")
             .attr("width", width)
             .attr("height", height)
-            .style("background", "black") // Set background to black
+            //.style("background", "black") // Set background to black
             .append("g")
             .attr("transform", `translate(${width / 2},${height / 2})`);
 
@@ -50,7 +65,7 @@ function processData(csvFile, containerId, title) {
             .attr("x", 0)
             .attr("y", -height / 2 + 20) // Adjust this value to move the title up or down
             .attr("text-anchor", "middle")
-            .attr("fill", "#fff")
+            .attr("fill", "black")
             .attr("font-size", "20px")
             .attr("font-weight", "bold")
             .text(title);
@@ -70,7 +85,7 @@ function processData(csvFile, containerId, title) {
 
         const color = d3.scaleOrdinal()
             .domain(["twoPointShots", "threePointShots"])
-            .range(["white", "yellow"]);
+            .range(["blue", "green"]); //TODO: colors
 
         const size = d3.scaleLinear()
             .domain([0, d3.max(flatData, d => Math.max(d.twoPointShots, d.threePointShots))])
@@ -92,8 +107,8 @@ function processData(csvFile, containerId, title) {
                             .html(`Date: ${d.date}<br>${shotType === 'twoPointShots' ? '2 Point Shots' : '3 Point Shots'}: ${d[shotType]}`);
                     })
                     .on("mousemove", function(event) {
-                        tooltip.style("top", (event.pageY - 10) + "px")
-                            .style("left", (event.pageX + 10) + "px");
+                        tooltip.style("top", (d3.event.pageY - 10) + "px")
+                            .style("left", (d3.event.pageX + 10) + "px");
                     })
                     .on("mouseout", function() {
                         d3.select(this).attr("stroke", null);
@@ -103,18 +118,18 @@ function processData(csvFile, containerId, title) {
         });
         // Add annotations at the start of each ring
         const annotations = [
-            { label: "2 Point Shots", angle: -0.5 * Math.PI, radius: shotRadius("twoPointShots") },
-            { label: "3 Point Shots", angle: -0.5 * Math.PI, radius: shotRadius("threePointShots") }
+            { label: "2 Points", angle: -0.5 * Math.PI, radius: shotRadius("twoPointShots") },
+            { label: "3 Points", angle: -0.5 * Math.PI, radius: shotRadius("threePointShots") }
         ];
 
         annotations.forEach(d => {
             svg.append("text")
-                .attr("x", Math.cos(d.angle) * d.radius)
+                .attr("x", Math.cos(d.angle) * d.radius - 45)
                 .attr("y", Math.sin(d.angle) * d.radius)
                 .attr("dy", "0.35em")
                 .attr("text-anchor", "middle")
-                .attr("fill", "#fff")
-                .attr("transform", `translate(-10, 0) rotate(90, ${Math.cos(d.angle) * d.radius}, ${Math.sin(d.angle) * d.radius})`)
+                .attr("fill", "black")
+                //.attr("transform", `translate(-100, 0))` ) // rotate(90, ${Math.cos(d.angle) * d.radius}, ${Math.sin(d.angle) * d.radius})`)
                 .text(d.label);
         });
 
@@ -127,12 +142,12 @@ function processData(csvFile, containerId, title) {
                 .attr("cx", 10)
                 .attr("cy", i * 20)
                 .attr("r", size(d))
-                .attr("fill", "white")
+                .attr("fill", "black")
                 .attr("opacity", 0.7);
             sizeLegend.append("text")
                 .attr("x", 30)
                 .attr("y", 5 + i * 20)
-                .attr("fill", "#fff")
+                .attr("fill", "black")
                 .text(d)
                 .attr("alignment-baseline", "middle");
         });
@@ -140,7 +155,7 @@ function processData(csvFile, containerId, title) {
         sizeLegend.append("text")
             .attr("x", 10)
             .attr("y", -10)
-            .attr("fill", "#fff")
+            .attr("fill", "black")
             .text("Shot Counts")
             .attr("alignment-baseline", "middle")
             .attr("font-weight", "bold");
@@ -168,12 +183,37 @@ function processData(csvFile, containerId, title) {
                 .attr("x", Math.cos(Math.PI) * d.radius)
                 .attr("y", Math.sin(Math.PI) * d.radius - barHeight - histogramOffset - 5)
                 .attr("text-anchor", "middle")
-                .attr("fill", "#fff")
+                .attr("fill", "black")
                 .text(d.count);
         });
 
     });
 }
-processData("../Data/2_james_harden_shot_chart_2023.csv", "#chart1", "James Harden 2023 Statistics");
-processData("../Data/1_lebron_james_shot_chart_1_2023.csv", "#chart2", "LeBron James 2023 Statistics");
-processData("../Data/3_stephen_curry_shot_chart_2023.csv", "#chart3", "Stephen Curry 2023 Statistics");
+
+//def a function to clear the chart
+function clearChart(containerId){
+    d3.select(containerId).selectAll("*").remove();
+}
+
+
+function changePlayer2(newPlayer){
+    clearChart("#chart1");
+    clearChart("#chart2");
+    clearChart("#chart3");
+
+    if (newPlayer == 'james'){
+        processData("/Data/1_lebron_james_shot_chart_1_2023.csv", "#chart2", "LeBron James 2023 Statistics");
+        
+    }
+    else if (newPlayer == 'curry'){
+        processData("/Data/3_stephen_curry_shot_chart_2023.csv", "#chart3", "Stephen Curry 2023 Statistics");
+        
+    }
+    else if (newPlayer == 'harden'){
+        processData("/Data/2_james_harden_shot_chart_2023.csv", "#chart1", "James Harden 2023 Statistics");
+
+    }
+    else if (newPlayer == 'empty'){
+
+    }
+}
