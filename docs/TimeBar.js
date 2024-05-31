3fd63c0cdd09b273ba1cf450da9a6af39780552f
psotogomez <p.soto.g@uc.cl>
2024-05-31 14:33:12 +0200

    clean code

--- docs/TimeBar.js
+++ docs/TimeBar.js
@@ -1,160 +1,116 @@
-//var playerButton = document.getElementById('playerButton');
-var playerSelect = document.getElementById('player');
-//var playerChoice = document.getElementById('playerChoice');
+var playerSelect = document.getElementById("player");
 
-// player switch
-// playerButton.addEventListener('click', function () {
-//     playerSelect.style.display = 'block';
-// });
-
-
-//playerSelect.addEventListener('change', function () {
 function changePlayer3(newPlayer) {
+  const svg = d3.select("#chart");
+  svg.selectAll("path").remove();
 
-    //playerChoice.textContent = playerSelect.value;
-    //playerSelect.style.display = 'none';
+  let csvFile;
+  switch (newPlayer) {
+    case "james":
+      console.log("james");
+      csvFile = "Data/james.csv";
+      break;
+    case "harden":
+      console.log("harden");
+      csvFile = "Data/harden.csv";
+      break;
+    case "curry":
+      console.log("curry");
+      csvFile = "Data/curry.csv";
+      break;
+    default:
+      console.error("Unknown player: ");
+      return;
+  }
 
-    // if (playerSelect.value === ' - ') {
-    // // Clear the playerChoice and data when "-" is selected
-    // //playerChoice.textContent = '';
-    // data = [];
-    // // Call a function to clear the displayed data/visualization
-    // clearVisualization();
-    // return;
-    // }
-    const svg = d3.select("#chart");
-    svg.selectAll("path").remove();
-    
+  d3.csv(csvFile, function (loadedData) {
+    loadedData.forEach(function (d) {
+      var timeComponents = d.time_remaining.split(":");
+      var minutes = parseInt(timeComponents[0]);
+      var seconds = parseInt(timeComponents[1]);
+      var totalTimeInSeconds = minutes * 60 + seconds;
 
-    let csvFile;
-    switch (newPlayer) {
-        case 'james':
-            console.log('james');
-            csvFile = 'Data/james.csv';
-            break;
-        case 'harden':
-            console.log('harden');
-            csvFile = 'Data/harden.csv';
-            break;
-        case 'curry':
-            console.log('curry');
-            csvFile = 'Data/curry.csv';
-            break;
-        default:
-            console.error('Unknown player: ');
-            return;
-    }
+      d.normalized_time = (10 * totalTimeInSeconds) / (12 * 60);
 
-    // use D3.js open csv file 
-    d3.csv(csvFile, function(loadedData) {
-        loadedData.forEach(function(d) {
-            var timeComponents = d.time_remaining.split(":");
-            var minutes = parseInt(timeComponents[0]);
-            var seconds = parseInt(timeComponents[1]);
-            var totalTimeInSeconds = minutes * 60 + seconds;
+      d.distance = (+d.distance_ft / 40) * 1100 + 205;
 
-            // time_remaining 
-            d.normalized_time = 10 * totalTimeInSeconds / (12 * 60); // 到 10s
-            // distance
-            d.distance = ((+d.distance_ft / 40) * 1100) + 205;
-            // result
-            d.result = d.result === 'TRUE';
-        });
-
-        data = loadedData;
-
+      d.result = d.result === "TRUE";
     });
+
+    data = loadedData;
+  });
 }
-//);
 
-
-//TODO: Change this to the css and add a hover and state change
 var startButton = document.getElementById("startButton");
-
-// startButton.addEventListener("mousedown", function () {
-//     startButton.style.background = "linear-gradient(to bottom, #069798, #0ABAB5)";
-// });
-
-// startButton.addEventListener("mouseup", function () {
-//     startButton.style.background = "linear-gradient(to bottom, #0ABAB5, #069798)";
-// });
 
 var timeBar = document.getElementById("timeBar");
 
 var interval;
 
-// startButton 
 let data;
 
-//divide data into the intervals
-// function divideData(data) {
-//     var intervals = [];
-//     window.interval = setInterval(function () {
-//         if (parseFloat(0) < timeBar.max) {
-//             timeBar.value = parseFloat(timeBar.value) + 0.1;
-//             updatePaths(timeBar.value);
-//         }
-//     }, 100);
-// }
 document.getElementById("startButton").addEventListener("click", function () {
-    console.log('press start')
-    const svg = d3.select("#chart");
-    svg.selectAll("path").remove();
-    svg.selectAll("path").remove();
-    if (parseFloat(timeBar.value) == timeBar.max) {
-        timeBar.value = timeBar.min;
+  console.log("press start");
+  const svg = d3.select("#chart");
+  svg.selectAll("path").remove();
+  if (parseFloat(timeBar.value) == timeBar.max) {
+    timeBar.value = timeBar.min;
+  }
+
+  clearInterval(window.interval);
+
+  var increment = 0.5;
+
+  window.interval = setInterval(function () {
+    console.log(timeBar.value);
+    if (parseFloat(timeBar.value) < timeBar.max) {
+      timeBar.value = parseFloat(timeBar.value) + increment;
+      updatePaths(timeBar.value);
+    } else {
+      clearInterval(window.interval);
     }
-    console.log("timeBarmax");
-    console.log(timeBar.max);
-
-    clearInterval(window.interval);
-
-    var increment = 0.5;
-
-    window.interval = setInterval(function () {
-        console.log(timeBar.value);
-        if (parseFloat(timeBar.value) < timeBar.max) {
-            timeBar.value = parseFloat(timeBar.value) + increment;
-            updatePaths(timeBar.value);
-        } else {
-            clearInterval(window.interval);
-        }
-    }, 100);
-
+  }, 100);
 });
 
 function updatePaths(timeValue) {
-    console.log("updatePaths: start")
-    const svg = d3.select("#chart");
+  console.log("updatePaths: start");
+  const svg = d3.select("#chart");
 
-    //svg.selectAll("path").remove();
+  const numPaths = 1;
+  const offset = 5;
+  const opacityStep = 0.2;
 
-    const numPaths = 1;  
-    const offset = 5;   
-    const opacityStep = 0.2;  
+  data.forEach(function (d) {
+    if (d.normalized_time * 100 <= timeValue) {
+      for (let i = 0; i < numPaths; i++) {
+        var path = svg
+          .append("path")
+          .attr("fill", "none")
+          .attr(
+            "stroke",
+            d.result
+              ? "rgba(0, 0, 245, " + opacityStep + ")"
+              : "rgba(234, 51, 35, " + opacityStep + ")"
+          )
+          .attr("stroke-width", 2)
+          .attr(
+            "d",
+            `M${d.distance + i * offset},450 Q${
+              (200 + d.distance + i * offset) / 2
+            },100 200,180`
+          );
 
-    data.forEach(function(d) {
-        if (d.normalized_time * 100 <= timeValue) {
-            for (let i = 0; i < numPaths; i++) {
-                //console.log(d.result); //TRUE , FALSE 
-                var path = svg.append("path")
-                    .attr("fill", "none")
-                    //.attr("stroke", d.color)
-                    .attr("stroke", d.result ? "rgba(0, 0, 245, " + ( opacityStep) + ")" : "rgba(234, 51, 35, " + (opacityStep) + ")")
-                    .attr("stroke-width", 2)
-                    .attr("d", `M${d.distance + i * offset},450 Q${(200 + d.distance + i * offset) / 2},100 200,180`);
+        var totalLength = path.node().getTotalLength();
 
-                var totalLength = path.node().getTotalLength();
-
-                path
-                    .attr("stroke-dasharray", totalLength + " " + totalLength)
-                    .attr("stroke-dashoffset", totalLength)
-                    .transition()
-                        .duration(150)
-                        .ease(d3.easeLinear)
-                        .attr("stroke-dashoffset", 0);
-            }
-        }
-    });
-    console.log("updatePaths: finish")
+        path
+          .attr("stroke-dasharray", totalLength + " " + totalLength)
+          .attr("stroke-dashoffset", totalLength)
+          .transition()
+          .duration(150)
+          .ease(d3.easeLinear)
+          .attr("stroke-dashoffset", 0);
+      }
+    }
+  });
+  console.log("updatePaths: finish");
 }
--- docs/radial_vis.js
+++ docs/radial_vis.js
@@ -1,219 +1,231 @@
 function processData(csvFile, containerId, title) {
-    d3.csv(csvFile, function (error, data) {
-    // d3.csv(csvFile).then(data => {
-        data.forEach(d => {
-            d['distance_ft'] = +d['distance_ft'];
-            d['lebron_team_score'] = +d['lebron_team_score'];
-            d['opponent_team_score'] = +d['opponent_team_score'];
-        });
-
-        const successfulShots = data.filter(d => d['result'].toLowerCase() === "true");
-
-
-        const shotCounts = d3.nest()
-            .key(d => d.date)
-            .rollup(v => ({
-                twoPointShots: v.filter(d => d.shot_type === '2').length,
-                threePointShots: v.filter(d => d.shot_type === '3').length
-            }))
-            .entries(successfulShots)
-        // const shotCounts = d3.rollups(successfulShots, v => ({
-        //     twoPointShots: v.filter(d => d['shot_type'] === '2').length,
-        //     threePointShots: v.filter(d => d['shot_type'] === '3').length
-        // }), d => d['date']);
-
-        const flatData = shotCounts.map(({ key, value }) => ({
-            date: key,
-            twoPointShots: value.twoPointShots,
-            threePointShots: value.threePointShots
-        }));
-
-        // const flatData = shotCounts.map(([date, counts]) => ({
-        //     date,
-        //     twoPointShots: counts.twoPointShots,
-        //     threePointShots: counts.threePointShots
-        // }));
-
-        console.log(flatData);
-
-        // Calculate total counts for each shot type
-        const totalCounts = flatData.reduce((acc, d) => {
-            acc.twoPointShots += d.twoPointShots;
-            acc.threePointShots += d.threePointShots;
-            return acc;
-        }, { twoPointShots: 0, threePointShots: 0 });
-
-        // Sort flatData by date
-        flatData.sort((a, b) => new Date(a.date) - new Date(b.date));
-
-        // Define SVG dimensions and radius for the rings
-        const width = 800; //800
-        const height = 700; //1000
-        const margin = { top: 50, right: 50, bottom: 50, left: 50 };
-        const radius = 1.25 * (Math.min(width, height) / 2 - Math.max(margin.top, margin.right, margin.bottom, margin.left));
-                    //1.25
-        // Create SVG element with black background
-        const svg = d3.select(containerId).append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            //.style("background", "black") // Set background to black
-            .append("g")
-            .attr("transform", `translate(${width / 2},${height / 2})`);
-
-        //Add title
-        // svg.append("text")
-        //     .attr("x", 0)
-        //     .attr("y", -height / 2 + 20) // Adjust this value to move the title up or down
-        //     .attr("text-anchor", "middle")
-        //     .attr("fill", "black")
-        //     .attr("font-size", "20px")
-        //     .attr("font-weight", "bold")
-        //     .text(title);
-
-        // Create tooltip
-        const tooltip = d3.select("body").append("div")
-            .attr("class", "tooltip");
-
-        // Define scales
-        const angle = d3.scaleTime()
-            .domain(d3.extent(flatData, d => new Date(d.date)))
-            .range([-0.5 * Math.PI, Math.PI]); // Adjust the range to π to 2.5π
-
-        const shotRadius = d3.scaleOrdinal()
-            .domain(["twoPointShots", "threePointShots"])
-            .range([radius * 0.5, radius * 0.75]);
-
-        const color = d3.scaleOrdinal()
-            .domain(["twoPointShots", "threePointShots"])
-            .range(["blue", "green"]); //TODO: colors
-
-        const size = d3.scaleLinear()
-            .domain([0, d3.max(flatData, d => Math.max(d.twoPointShots, d.threePointShots))])
-            .range([2, 10]); // Adjust the range based on your preference
-
-        // Plot data points
-        flatData.forEach(d => {
-            ["twoPointShots", "threePointShots"].forEach((shotType) => {
-                const circle = svg.append("circle")
-                    .attr("cx", Math.cos(angle(new Date(d.date))) * shotRadius(shotType))
-                    .attr("cy", Math.sin(angle(new Date(d.date))) * shotRadius(shotType))
-                    .attr("r", size(d[shotType]))
-                    .attr("fill", color(shotType))
-                    .attr("opacity", 0.7);
-
-                circle.on("mouseover", function(event) {
-                        d3.select(this).attr("stroke", "yellow").attr("stroke-width", 2);
-                        tooltip.style("visibility", "visible")
-                            .html(`Date: ${d.date}<br>${shotType === 'twoPointShots' ? '2 Point Shots' : '3 Point Shots'}: ${d[shotType]}`);
-                    })
-                    .on("mousemove", function(event) {
-                        tooltip.style("top", (d3.event.pageY - 10) + "px")
-                            .style("left", (d3.event.pageX + 10) + "px");
-                    })
-                    .on("mouseout", function() {
-                        d3.select(this).attr("stroke", null);
-                        tooltip.style("visibility", "hidden");
-                    });
-            });
-        });
-        // Add annotations at the start of each ring
-        const annotations = [
-            { label: "2 Points", angle: -0.5 * Math.PI, radius: shotRadius("twoPointShots") },
-            { label: "3 Points", angle: -0.5 * Math.PI, radius: shotRadius("threePointShots") }
-        ];
-
-        annotations.forEach(d => {
-            svg.append("text")
-                .attr("x", Math.cos(d.angle) * d.radius - 45)
-                .attr("y", Math.sin(d.angle) * d.radius)
-                .attr("dy", "0.35em")
-                .attr("text-anchor", "middle")
-                .attr("fill", "black")
-                //.attr("transform", `translate(-100, 0))` ) // rotate(90, ${Math.cos(d.angle) * d.radius}, ${Math.sin(d.angle) * d.radius})`)
-                .text(d.label);
-        });
-
-        // Add size legend
-        const sizeLegend = svg.append("g")
-            .attr("transform", `translate(${-(width / 2 - 50)}, ${-(height / 2 - 50)})`); // Adjust this line to move the legend to the top-left
-
-        [0, 5, 10].forEach((d, i) => {
-            sizeLegend.append("circle")
-                .attr("cx", 10)
-                .attr("cy", i * 20)
-                .attr("r", size(d))
-                .attr("fill", "black")
-                .attr("opacity", 0.7);
-            sizeLegend.append("text")
-                .attr("x", 30)
-                .attr("y", 5 + i * 20)
-                .attr("fill", "black")
-                .text(d)
-                .attr("alignment-baseline", "middle");
-        });
-
-        sizeLegend.append("text")
-            .attr("x", 10)
-            .attr("y", -10)
-            .attr("fill", "black")
-            .text("Shot Counts")
-            .attr("alignment-baseline", "middle")
-            .attr("font-weight", "bold");
-
-        // Add histograms at π position for each ring
-        const histogramWidth = 10;
-        const histogramHeight = 80;
-        const histogramOffset = 20; // Offset to move histogram upwards
-
-        const histogramData = [
-            { label: "2 Point Shots", count: totalCounts.twoPointShots, radius: shotRadius("twoPointShots") },
-            { label: "3 Point Shots", count: totalCounts.threePointShots, radius: shotRadius("threePointShots") }
-        ];
-
-        histogramData.forEach((d, i) => {
-            const barHeight = d.count / d3.max(histogramData, h => h.count) * histogramHeight;
-            svg.append("rect")
-                .attr("x", Math.cos(Math.PI) * d.radius - histogramWidth / 2)
-                .attr("y", Math.sin(Math.PI) * d.radius - barHeight - histogramOffset)
-                .attr("width", histogramWidth)
-                .attr("height", barHeight)
-                .attr("fill", color(d.label.split(' ')[0].toLowerCase() + 'Shots'));
-
-            svg.append("text")
-                .attr("x", Math.cos(Math.PI) * d.radius)
-                .attr("y", Math.sin(Math.PI) * d.radius - barHeight - histogramOffset - 5)
-                .attr("text-anchor", "middle")
-                .attr("fill", "black")
-                .text(d.count);
-        });
-
+  d3.csv(csvFile, function (error, data) {
+    data.forEach((d) => {
+      d["distance_ft"] = +d["distance_ft"];
+      d["lebron_team_score"] = +d["lebron_team_score"];
+      d["opponent_team_score"] = +d["opponent_team_score"];
     });
+
+    const successfulShots = data.filter(
+      (d) => d["result"].toLowerCase() === "true"
+    );
+
+    const shotCounts = d3
+      .nest()
+      .key((d) => d.date)
+      .rollup((v) => ({
+        twoPointShots: v.filter((d) => d.shot_type === "2").length,
+        threePointShots: v.filter((d) => d.shot_type === "3").length,
+      }))
+      .entries(successfulShots);
+
+    const flatData = shotCounts.map(({ key, value }) => ({
+      date: key,
+      twoPointShots: value.twoPointShots,
+      threePointShots: value.threePointShots,
+    }));
+
+    const totalCounts = flatData.reduce(
+      (acc, d) => {
+        acc.twoPointShots += d.twoPointShots;
+        acc.threePointShots += d.threePointShots;
+        return acc;
+      },
+      { twoPointShots: 0, threePointShots: 0 }
+    );
+
+    flatData.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+    const width = 800;
+    const height = 700;
+    const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+    const radius =
+      1.25 *
+      (Math.min(width, height) / 2 -
+        Math.max(margin.top, margin.right, margin.bottom, margin.left));
+
+    const svg = d3
+      .select(containerId)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", `translate(${width / 2},${height / 2})`);
+
+    const tooltip = d3.select("body").append("div").attr("class", "tooltip");
+
+    const angle = d3
+      .scaleTime()
+      .domain(d3.extent(flatData, (d) => new Date(d.date)))
+      .range([-0.5 * Math.PI, Math.PI]);
+
+    const shotRadius = d3
+      .scaleOrdinal()
+      .domain(["twoPointShots", "threePointShots"])
+      .range([radius * 0.5, radius * 0.75]);
+
+    const color = d3
+      .scaleOrdinal()
+      .domain(["twoPointShots", "threePointShots"])
+      .range(["blue", "green"]);
+
+    const size = d3
+      .scaleLinear()
+      .domain([
+        0,
+        d3.max(flatData, (d) => Math.max(d.twoPointShots, d.threePointShots)),
+      ])
+      .range([2, 10]);
+
+    flatData.forEach((d) => {
+      ["twoPointShots", "threePointShots"].forEach((shotType) => {
+        const circle = svg
+          .append("circle")
+          .attr("cx", Math.cos(angle(new Date(d.date))) * shotRadius(shotType))
+          .attr("cy", Math.sin(angle(new Date(d.date))) * shotRadius(shotType))
+          .attr("r", size(d[shotType]))
+          .attr("fill", color(shotType))
+          .attr("opacity", 0.7);
+
+        circle
+          .on("mouseover", function (event) {
+            d3.select(this).attr("stroke", "yellow").attr("stroke-width", 2);
+            tooltip
+              .style("visibility", "visible")
+              .html(
+                `Date: ${d.date}<br>${
+                  shotType === "twoPointShots"
+                    ? "2 Point Shots"
+                    : "3 Point Shots"
+                }: ${d[shotType]}`
+              );
+          })
+          .on("mousemove", function (event) {
+            tooltip
+              .style("top", d3.event.pageY - 10 + "px")
+              .style("left", d3.event.pageX + 10 + "px");
+          })
+          .on("mouseout", function () {
+            d3.select(this).attr("stroke", null);
+            tooltip.style("visibility", "hidden");
+          });
+      });
+    });
+
+    const annotations = [
+      {
+        label: "2 Points",
+        angle: -0.5 * Math.PI,
+        radius: shotRadius("twoPointShots"),
+      },
+      {
+        label: "3 Points",
+        angle: -0.5 * Math.PI,
+        radius: shotRadius("threePointShots"),
+      },
+    ];
+
+    annotations.forEach((d) => {
+      svg
+        .append("text")
+        .attr("x", Math.cos(d.angle) * d.radius - 45)
+        .attr("y", Math.sin(d.angle) * d.radius)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "middle")
+        .attr("fill", "black")
+        .text(d.label);
+    });
+
+    const sizeLegend = svg
+      .append("g")
+      .attr(
+        "transform",
+        `translate(${-(width / 2 - 50)}, ${-(height / 2 - 50)})`
+      );
+
+    [0, 5, 10].forEach((d, i) => {
+      sizeLegend
+        .append("circle")
+        .attr("cx", 10)
+        .attr("cy", i * 20)
+        .attr("r", size(d))
+        .attr("fill", "black")
+        .attr("opacity", 0.7);
+      sizeLegend
+        .append("text")
+        .attr("x", 30)
+        .attr("y", 5 + i * 20)
+        .attr("fill", "black")
+        .text(d)
+        .attr("alignment-baseline", "middle");
+    });
+
+    sizeLegend
+      .append("text")
+      .attr("x", 10)
+      .attr("y", -10)
+      .attr("fill", "black")
+      .text("Shot Counts")
+      .attr("alignment-baseline", "middle")
+      .attr("font-weight", "bold");
+
+    const histogramWidth = 10;
+    const histogramHeight = 80;
+    const histogramOffset = 20;
+
+    const histogramData = [
+      {
+        label: "2 Point Shots",
+        count: totalCounts.twoPointShots,
+        radius: shotRadius("twoPointShots"),
+      },
+      {
+        label: "3 Point Shots",
+        count: totalCounts.threePointShots,
+        radius: shotRadius("threePointShots"),
+      },
+    ];
+
+    histogramData.forEach((d, i) => {
+      const barHeight =
+        (d.count / d3.max(histogramData, (h) => h.count)) * histogramHeight;
+      svg
+        .append("rect")
+        .attr("x", Math.cos(Math.PI) * d.radius - histogramWidth / 2)
+        .attr("y", Math.sin(Math.PI) * d.radius - barHeight - histogramOffset)
+        .attr("width", histogramWidth)
+        .attr("height", barHeight)
+        .attr("fill", color(d.label.split(" ")[0].toLowerCase() + "Shots"));
+
+      svg
+        .append("text")
+        .attr("x", Math.cos(Math.PI) * d.radius)
+        .attr(
+          "y",
+          Math.sin(Math.PI) * d.radius - barHeight - histogramOffset - 5
+        )
+        .attr("text-anchor", "middle")
+        .attr("fill", "black")
+        .text(d.count);
+    });
+  });
 }
 
-//def a function to clear the chart
-function clearChart(containerId){
-    d3.select(containerId).selectAll("*").remove();
+function clearChart(containerId) {
+  d3.select(containerId).selectAll("*").remove();
 }
 
+function changePlayer2(newPlayer) {
+  clearChart("#chart1");
+  clearChart("#chart2");
+  clearChart("#chart3");
 
-function changePlayer2(newPlayer){
-    clearChart("#chart1");
-    clearChart("#chart2");
-    clearChart("#chart3");
-
-    if (newPlayer == 'james'){
-        processData("Data/james.csv", "#chart2", "LeBron James 2023 Statistics");
-        
-    }
-    else if (newPlayer == 'curry'){
-        processData("Data/curry.csv", "#chart3", "Stephen Curry 2023 Statistics");
-        
-    }
-    else if (newPlayer == 'harden'){
-        processData("Data/harden.csv", "#chart1", "James Harden 2023 Statistics");
-
-    }
-    else if (newPlayer == 'empty'){
-
-    }
+  if (newPlayer == "james") {
+    processData("Data/james.csv", "#chart2", "LeBron James 2023 Statistics");
+  } else if (newPlayer == "curry") {
+    processData("Data/curry.csv", "#chart3", "Stephen Curry 2023 Statistics");
+  } else if (newPlayer == "harden") {
+    processData("Data/harden.csv", "#chart1", "James Harden 2023 Statistics");
+  } else if (newPlayer == "empty") {
+  }
 }
